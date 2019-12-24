20034d4ba26f11cc6d05cc414a2b3bc4c24f1851
xtha1 <shankarxtha@gmail.com>
2019-12-24 16:53:19 +0545

    Done

---
+++ Assignment/Box Render/rectangle.js
@@ -0,0 +1,15 @@
+var red= document.getElementById("bob");
+red.style.width="150px";
+red.style.height="100px";
+red.style.background="red";
+red.style.left="10px";
+red.style.position="absolute";
+document.body.appendChild(red);
+
+var yellow = document.getElementById("demon")
+yellow.style.width="150px";
+yellow.style.height="100px";
+yellow.style.right="950px";
+yellow.style.position="absolute";
+yellow.style.background="yellow";
+document.body.appendChild(yellow);
