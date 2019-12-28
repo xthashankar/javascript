d0d24d3c7ed33b120e1ae3e0315889b09d727e85
xtha1 <shankarxtha@gmail.com>
2019-12-28 18:14:31 +0545

    moving box update

---
+++ Assignment/Moving Box/Box.js
@@ -0,0 +1,32 @@
+var red=document.getElementById("bob");
+red.style.width="150px";
+red.style.height="100px";
+red.style.background="red";
+red.style.left="0px";
+red.style.top='0px';
+red.style.position="absolute";
+// document.body.appendChild(red);
+
+document.body.onkeyup=function()
+{
+    var e=event.keyCode,
+    Top=parseInt(document.getElementById("bob").style.top);
+    Left=parseInt(document.getElementById("bob").style.left);
+
+    if(e==40)//down function 
+    { 
+        document.getElementById("bob").style.top=(Top)+10+"px";
+    } else if(e==37)//left function
+    { 
+        document.getElementById("bob").style.left=(Left)-10+"px";
+    }
+    else if(e==39)//right function
+    {
+        document.getElementById("bob").style.left = (Left)+10+"px";
+    } 
+    else if(e==38)//up function
+     { 
+        document.getElementById("bob").style.top = (Top)-10+"px";
+    }
+
+}
