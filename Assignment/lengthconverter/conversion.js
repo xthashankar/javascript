7ee736b7f43e05246a1b9ec731773786f16c5e79
xtha1 <shankarxtha@gmail.com>
2020-03-12 22:15:48 +0545

    learning

---
+++ Assignment/lengthconverter/conversion.js
@@ -0,0 +1,42 @@
+
+var textbox=document.getElementById('textbox');
+var textbox1=document.getElementById('textbox1');
+var dropdown =document.getElementById('dropdown');
+var dropdown1=document.getElementById('dropdown1');
+
+var dropdownvalue=dropdown.value;
+var dropdown1value=dropdown1.value;
+
+textbox.addEventListener("keypress",converter);
+dropdown.addEventListener("change",converter);
+dropdown1.addEventListener("change",converter);
+
+function converter()
+{
+    dropdownvalue=dropdown.value;
+    dropdown1value=dropdown1.value
+    if(dropdownvalue==="cm" && dropdown1value==="km")
+    {
+        textbox1.value=Number(textbox.value)/100000;
+    }
+    else if(dropdownvalue==="cm" &&dropdown1value==="mm")
+    {
+        textbox1.value=Number(textbox.value)*10;
+    }
+    if(dropdownvalue==="km" && dropdown1value==="cm")
+    {
+        textbox1.value=Number(textbox.value)*100000;
+    }
+    else if(dropdownvalue==="km" &&dropdown1value==="mm")
+    {
+        textbox1.value=Number(textbox.value)*1000000;
+    }
+    if(dropdownvalue==="mm" && dropdown1value==="cm")
+    {
+        textbox1.value=Number(textbox.value)/10;
+    }
+    else if(dropdownvalue==="mm" &&dropdown1value==="km")
+    {
+        textbox1.value=Number(textbox.value)/1000000;
+    }
+}
